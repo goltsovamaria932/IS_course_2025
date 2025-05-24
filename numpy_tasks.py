2ee68b567a64ce73cb72be3f6bc6de0937c29e93
goltsovamaria932 <1132236080@pfur.ru>
2025-05-24 09:15:00 +0300

    Update numpy_tasks.py

--- numpy_tasks.py
+++ numpy_tasks.py
@@ -1,90 +1,67 @@
 import numpy as np
 
 def uniform_intervals(a, b, n):
-    
     return np.linspace(a, b, n)
-
-
+    pass
 
 def cyclic123_array(n):
-    
-    base_array = np.array([1, 2, 3])
-    return np.tile(base_array, n)
-
-
+    return np.tile([1, 2, 3], n)
+    pass
 
 def first_n_odd_number(n):
-    
-    return np.arange(1, 2*n, 2)
-
-
+    return np.arange(1, 2 * n, 2)
+    pass
 
 def zeros_array_with_border(n):
-   
-    Z = np.zeros((n,n))
-    Z[0,:] = 1
-    Z[-1,:] = 1
-    Z[:,0] = 1
-    Z[:,-1] = 1
-    return Z
-
-
+    arr = np.zeros((n, n), dtype=int)
+    arr[0, :] = 1      # Верхняя граница
+    arr[-1, :] = 1     # Нижняя граница
+    arr[:, 0] = 1      # Левая граница
+    arr[:, -1] = 1     # Правая граница
+    return arr
+    pass
 
 def chess_board(n):
-    
-
-    x = np.zeros((n,n),dtype=int)
-    x[1::2,::2] = 1
-    x[::2,1::2] = 1
-    return x
-
-
+    return (np.indices((n, n)).sum(axis=0)+1) % 2
+    pass
 
 def matrix_with_sum_index(n):
-    
-    rows = np.arange(n)
+    rows = np.arange(n).reshape(-1, 1)
     cols = np.arange(n)
-    return rows[:, np.newaxis] + cols
-
-
+    return rows + cols
+    pass
 
 def cos_sin_as_two_rows(a, b, dx):
-    
     x = np.arange(a, b, dx)
-    cos_x = np.cos(x)
-    sin_x = np.sin(x)
-    return np.array([cos_x, sin_x])
-
-
+    cos_vals = np.cos(x) 
+    sin_vals = np.sin(x)
+    return np.vstack((cos_vals, sin_vals))
+    pass
 
 def compute_mean_rowssum_columnssum(A):
-    
-    mean = np.mean(A)
-    rows_sum = np.sum(A, axis=1)
-    columns_sum = np.sum(A, axis=0)
-    return mean, rows_sum, columns_sum
-
-
+    return np.mean(A), np.sum(A, axis=0), np.sum(A, axis=1)
+    pass
 
 def sort_array_by_column(A, j):
-    
-    return A[A[:,j].argsort()]
-
-
+    return A[A[:, j].argsort()]
+    pass
 
 def compute_integral(a, b, f, dx, method):
-   
-    x = np.arange(a, b, dx)
+    x = np.arange(a, b + dx, dx)  # Точки интегрирования (включая правую границу)
+    y = f(x)
+    
     if method == 'rectangular':
-        return np.sum(f(x) * dx)
+        # Метод прямоугольников
+        return np.sum(y[:-1] * dx)
     elif method == 'trapezoidal':
-        return (dx/2) * (f(a) + 2*np.sum(f(x[1:])) + f(b-dx))
+        # Метод трапеций
+        return np.sum((y[:-1] + y[1:]) * dx / 2)
     elif method == 'simpson':
-        n = len(x)
-        if n % 2 == 0:
-            x = x[:-1] # make n odd
-            n = len(x)
-        h = (b - a) / (n - 1) # redefine h
-        return (h/3) * (f(a) + 4*np.sum(f(x[1::2])) + 2*np.sum(f(x[2:-1:2])) + f(b-dx))
-
-
+        # Метод Симпсона
+        if (len(x) - 1) % 2 != 0:
+            # Если число интервалов нечетное, уменьшаем шаг на последнем интервале
+            last_dx = b - x[-2]
+            simpson_part = (y[-3] + 4*y[-2] + y[-1]) * last_dx / 6
+            return (np.sum(y[:-3:2] + 4*y[1:-2:2] + y[2:-1:2]) * dx / 3) + simpson_part
+        return np.sum(y[:-1:2] + 4*y[1::2] + y[2::2]) * dx / 3
+        pass
